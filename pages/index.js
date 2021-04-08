e86079f1af6440c21d35c4a2e3ebfb7cb44f37a6
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-08 11:10:56 -0700

    initialize project and add landing page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,142 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className="bg-gray-50">
+        <div className="relative overflow-hidden">
+          <div className="absolute inset-y-0 h-full w-full" aria-hidden="true">
+            <div className="relative h-full">
+              <svg
+                className="absolute right-full transform translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full"
+                width={404}
+                height={784}
+                fill="none"
+                viewBox="0 0 404 784"
+              >
+                <defs>
+                  <pattern
+                    id="e229dbec-10e9-49ee-8ec3-0286ca089edf"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect width={404} height={784} fill="url(#e229dbec-10e9-49ee-8ec3-0286ca089edf)" />
+              </svg>
+              <svg
+                className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
+                width={404}
+                height={784}
+                fill="none"
+                viewBox="0 0 404 784"
+              >
+                <defs>
+                  <pattern
+                    id="d2a68204-c383-44b1-b99f-42ccff4e5365"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect width={404} height={784} fill="url(#d2a68204-c383-44b1-b99f-42ccff4e5365)" />
+              </svg>
+            </div>
+          </div>
+          <div className="relative pt-6 pb-16 sm:pb-24">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+              <nav
+                className="relative flex items-center justify-between sm:h-10 md:justify-center"
+                aria-label="Global"
+              >
+                <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <a href="#">
+                      <span className="sr-only">Workflow</span>
+                      <img
+                        className="h-8 w-auto sm:h-10"
+                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                        alt
+                      />
+                    </a>
+                  </div>
+                </div>
+              </nav>
+            </div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+              <div className="text-center">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block">The easiest way to create a</span>
+                  <span className="block text-indigo-600">README</span>
+                </h1>
+                <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                  Our simple editor allows you to quickly add and customize all the sections you
+                  need for your project's readme
+                </p>
+              </div>
+              <div className="flex justify-center mt-6">
+                <span className="inline-flex rounded-md shadow">
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500"
+                  >
+                    Get Started
+                  </a>
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="mt-3 mx-auto text-sm text-gray-900">100% Free</p>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="absolute inset-0 flex flex-col" aria-hidden="true">
+              <div className="flex-1" />
+              <div className="flex-1 w-full bg-gray-800" />
+            </div>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+              <img
+                className="relative rounded-lg shadow-lg"
+                src="https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg"
+                alt="App screenshot"
+              />
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        <div className="bg-gray-800">
+          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            <h2 className="text-center text-gray-400 text-sm font-semibold tracking-wide">
+              Made with &#9825; by{' '}
+              <a className="hover:text-indigo-600" href="https://twitter.com/katherinecodes">
+                Katherine Peterson
+              </a>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
