2f63cd785f446d80a7c43c78baba7f757b2c8e77
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-08 16:41:30 -0700

    add markdown library

--- pages/editor.js
+++ pages/editor.js
@@ -1,6 +1,10 @@
 import Link from 'next/link'
+import ReactMarkdown from 'react-markdown'
+import { useState } from 'react'
+const gfm = require('remark-gfm')
 
 export default function Editor() {
+  const [markdown, setMarkdown] = useState('')
   return (
     <>
       <nav className="bg-gray-800 flex justify-between p-4 align-center">
@@ -54,10 +58,14 @@ export default function Editor() {
               name="markdown"
               rows="12"
               class="shadow-sm block w-full focus:ring-green-500 focus:border-green-500 sm:text-sm border-gray-800 rounded-md p-6 bg-gray-700 text-white"
+              onChange={(e) => setMarkdown(e.target.value)}
             ></textarea>
           </div>
           <div className="px-3 w-1/2 flex-1">
             <h3 className="text-lg leading-6 font-medium text-gray-900 mb-3">Preview</h3>
+            <div className="h-full border border-gray-500 rounded-md p-6">
+              <ReactMarkdown plugins={[gfm]}>{markdown}</ReactMarkdown>
+            </div>
           </div>
         </div>
       </div>
