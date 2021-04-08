1ea40daf7b62a5d68d89cb60f5f0b2f50244f902
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-08 11:26:18 -0700

    update landing page colors and logo

--- pages/index.js
+++ pages/index.js
@@ -75,11 +75,7 @@ export default function Home() {
                   <div className="flex items-center justify-between w-full md:w-auto">
                     <a href="#">
                       <span className="sr-only">Workflow</span>
-                      <img
-                        className="h-8 w-auto sm:h-10"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt
-                      />
+                      <img className="h-8 w-auto sm:h-10" src="readme.svg" alt />
                     </a>
                   </div>
                 </div>
@@ -90,7 +86,7 @@ export default function Home() {
               <div className="text-center">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                   <span className="block">The easiest way to create a</span>
-                  <span className="block text-indigo-600">README</span>
+                  <span className="block text-blue-600">README</span>
                 </h1>
                 <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                   Our simple editor allows you to quickly add and customize all the sections you
@@ -101,7 +97,7 @@ export default function Home() {
                 <span className="inline-flex rounded-md shadow">
                   <a
                     href="#"
-                    className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:text-indigo-500"
                   >
                     Get Started
                   </a>
@@ -130,7 +126,7 @@ export default function Home() {
           <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
             <h2 className="text-center text-gray-400 text-sm font-semibold tracking-wide">
               Made with &#9825; by{' '}
-              <a className="hover:text-indigo-600" href="https://twitter.com/katherinecodes">
+              <a className="hover:text-blue-600" href="https://twitter.com/katherinecodes">
                 Katherine Peterson
               </a>
             </h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      emerald: colors.emerald,
+      blue: colors.blue,
+    },
   },
   variants: {
     extend: {},
