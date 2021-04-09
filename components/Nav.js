09c31950601322419a66166901b0d793597f5f1a
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-09 13:02:04 -0700

    change export button to download button with icon

--- components/Nav.js
+++ components/Nav.js
@@ -3,13 +3,14 @@ import Link from 'next/link'
 export const Nav = () => (
   <nav className="bg-gray-800 flex justify-between p-4 align-center">
     <Link href="/">
-      <img className="h-8 mt-1 w-auto cursor-pointer" src="readme.svg" alt />
+      <img className="h-8 mt-1 w-auto cursor-pointer" src="readme.svg" />
     </Link>
     <button
       type="button"
       className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-bold tracking-wide rounded-md text-white bg-orange-500 hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-orange-500"
     >
-      <span>Export</span>
+      <img className="h-6 mr-2 w-auto cursor-pointer" src="download.svg" />
+      <span>Download</span>
     </button>
   </nav>
 )
