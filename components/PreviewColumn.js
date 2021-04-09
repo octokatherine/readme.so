d7d66e370b6368f76355b0922fb4d8465f583dc7
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-08 17:22:30 -0700

    start adding styles for markdown preview

--- components/PreviewColumn.js
+++ components/PreviewColumn.js
@@ -5,7 +5,7 @@ export const PreviewColumn = ({ markdown }) => {
   return (
     <div className="px-3 w-1/2 flex-1">
       <h3 className="text-lg leading-6 font-medium text-gray-900 mb-3">Preview</h3>
-      <div className="h-full border border-gray-500 rounded-md p-6">
+      <div className="h-full border border-gray-500 rounded-md p-6 preview">
         <ReactMarkdown plugins={[gfm]}>{markdown}</ReactMarkdown>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
