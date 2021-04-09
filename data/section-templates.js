088fe348d05789caa525c56a24a0dc626a61f81d
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-08 18:55:50 -0700

    add first couple markdown sections

---
+++ data/section-templates.js
@@ -0,0 +1,20 @@
+export const templates = [
+  {
+    title: 'Title and Description',
+    markdown: `
+      # Project Title
+      A brief description of what this project does and who it's for
+    `,
+  },
+  {
+    title: 'Installation',
+    markdown: `
+      ## Installation 
+      Install my-project with npm
+      \`\`\`bash 
+        npm install my-project
+        cd my-project
+      \`\`\`
+    `,
+  },
+]
---
+++ template.md
@@ -0,0 +1,12 @@
+# My Project
+
+A brief description of this project and who it's for
+
+## Installation
+
+Install my-project with npm
+
+```bash
+  npm install my-project
+  cd my-project
+```
