19af6939020b52ccc0b7c89bed05d709a19daa01
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-09 23:29:03 -0700

    add max height to sections column

--- components/SectionsColumn.js
+++ components/SectionsColumn.js
@@ -55,50 +55,52 @@ export const SectionsColumn = ({
   }
 
   return (
-    <div className="sections px-3">
+    <div className="sections">
       <h3 className="text-lg leading-6 font-medium text-gray-900 mb-3">Sections</h3>
-      {selectedSections.length > 0 && (
-        <h4 className="text-xs leading-6 text-gray-900 mb-3">
-          Click on a section below to edit the contents
-        </h4>
-      )}
-      <ul className="space-y-3 mb-12">
-        <DndContext
-          sensors={sensors}
-          collisionDetection={closestCenter}
-          onDragEnd={handleDragEnd}
-          modifiers={[restrictToVerticalAxis]}
-        >
-          <SortableContext items={selectedSections} strategy={verticalListSortingStrategy}>
-            {selectedSections.map((s) => (
-              <SortableItem
-                key={s}
-                id={s}
-                section={getTemplate(s)}
-                focusedSectionSlug={focusedSectionSlug}
-                setFocusedSectionSlug={setFocusedSectionSlug}
-                onDeleteSection={onDeleteSection}
-              />
-            ))}
-          </SortableContext>
-        </DndContext>
-      </ul>
-      {sections.length > 0 && (
-        <h4 className="text-xs leading-6 text-gray-900 mb-3">
-          Click on a section below to add it to your readme
-        </h4>
-      )}
-      <ul className="space-y-3 mb-12">
-        {sections.map((s) => (
-          <li
-            onClick={(e) => onAddSection(e, s)}
-            key={s}
-            className="bg-white shadow rounded-md pl-3 pr-6 py-2 flex items-center cursor-pointer"
+      <div className="max-h-screen overflow-scroll px-3 pr-4">
+        {selectedSections.length > 0 && (
+          <h4 className="text-xs leading-6 text-gray-900 mb-3">
+            Click on a section below to edit the contents
+          </h4>
+        )}
+        <ul className="space-y-3 mb-12">
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+            modifiers={[restrictToVerticalAxis]}
           >
-            <p>{getTemplate(s).name}</p>
-          </li>
-        ))}
-      </ul>
+            <SortableContext items={selectedSections} strategy={verticalListSortingStrategy}>
+              {selectedSections.map((s) => (
+                <SortableItem
+                  key={s}
+                  id={s}
+                  section={getTemplate(s)}
+                  focusedSectionSlug={focusedSectionSlug}
+                  setFocusedSectionSlug={setFocusedSectionSlug}
+                  onDeleteSection={onDeleteSection}
+                />
+              ))}
+            </SortableContext>
+          </DndContext>
+        </ul>
+        {sections.length > 0 && (
+          <h4 className="text-xs leading-6 text-gray-900 mb-3">
+            Click on a section below to add it to your readme
+          </h4>
+        )}
+        <ul className="space-y-3 mb-12">
+          {sections.map((s) => (
+            <li
+              onClick={(e) => onAddSection(e, s)}
+              key={s}
+              className="bg-white shadow rounded-md pl-3 pr-6 py-2 flex items-center cursor-pointer"
+            >
+              <p>{getTemplate(s).name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
