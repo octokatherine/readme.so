8a7be409fd19d840ef8dba7bd91ec8681d21f580
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2021-04-11 17:45:00 +0200

    fix: Change overflow to only vertical (#17)

--- components/PreviewColumn.js
+++ components/PreviewColumn.js
@@ -10,7 +10,7 @@ export const PreviewColumn = ({ selectedSectionSlugs, getTemplate }) => {
   return (
     <div className="px-3 flex-1 mt-6 lg:mt-0">
       <h3 className="text-lg leading-6 font-medium text-gray-900 mb-3 ">Preview</h3>
-      <div className="h-full border border-gray-500 rounded-md p-6 preview bg-white max-h-screen overflow-scroll">
+      <div className="h-full border border-gray-500 rounded-md p-6 preview bg-white max-h-screen overflow-y-scroll">
         <ReactMarkdown plugins={[gfm]} children={markdown} />
       </div>
     </div>
