09a3ec59c81ef952c6d659d35536832d32941fdd
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-04-11 17:57:55 +0200

    fix: Delete unnecessary Container

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 import { DefaultSeo } from 'next-seo'
-import App, { Container } from 'next/app'
+import App from 'next/app'
 
 import SEO from '../next-seo.config.js'
 
@@ -9,10 +9,10 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Container>
+      <>
         <DefaultSeo {...SEO} />
         <Component {...pageProps} />
-      </Container>
+      </>
     )
   }
 }
