a9ec5804c774cacd4b54dad6f7dca5f0d9b32000
Rohit Mathew <rohit.mathew@turtlemint.com>
2021-04-12 01:08:20 +0530

    Try changing twitter card types
    This commit tries to change the twitter card type and see if this is better

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,19 +1,28 @@
 export default {
 	defaultTitle: 'readme.so - Easiest Way to Create A README',
 	description: `Use readme.so's markdown editor and ready made templates to easily create a simple README for your repositories`,
+	additionalMetaTags: [{
+		property: 'keywords',
+		content: 'readme, easiest, create, markdown, git, github, gitlab'
+	}],
 	openGraph: {
 		type: 'website',
 		locale: 'en_US',
 		url: 'https://readme.so/',
 		title: 'readme.so - Easiest Way to Create A README',
 		description: `Use readme.so's markdown editor and ready made templates to easily create a simple README for your repositories`,
-		image: 'https://readme-so-git-fork-rohitjmathew-main-katherinepeterson.vercel.app/readme.png',
+		images: [{
+			url: 'https://www.example.ie/og-image.jpg',
+			width: 375,
+			height: 375,
+			alt: 'Og Image Alt',
+		}],
 		site_name: 'readme.so',
 		imageWidth: 1200,
 		imageHeight: 1200
 	},
 	twitter: {
 		handle: '@katherinecodes',
-		cardType: 'summary_large_image'
+		cardType: 'summary'
 	}
 };
