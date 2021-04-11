a642d650816834e31bafd87471a8da68f28cec36
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-04-11 18:00:41 +0200

    chore: Optimize Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      emerald: colors.emerald,
-      blue: colors.blue,
-      orange: colors.orange,
-      teal: colors.teal,
+    extend: {
+      colors: {
+        gray: colors.trueGray,
+        red: colors.rose,
+        yellow: colors.amber,
+      },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
