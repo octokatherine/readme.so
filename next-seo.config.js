8ec3ea626021982c97e50ab3c32035985d41fe24
Rohit Jacob Mathew <rohitjmathew@hotmail.com>
2021-04-11 14:19:01 +0530

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -6,7 +6,7 @@ export default {
         locale: 'en_US',
         url: 'https://readme.so/',
         title: 'readme.so - Easiest Way to Create A README',
-        description: 'Easiest Way to Create A README',
+        description: `Use readme.so's markdown editor and ready made templates to easily create a simple README for your repositories`,
         image: 'https://github.com/katherinepeterson/readme.so/blob/main/public/readme.png',
         site_name: 'readme.so',
         imageWidth: 1200,
