d2e836c021a74ed132b452a152ca38e4f3245d56
Rohit Jacob Mathew <rohit.mathew@turtlemint.com>
2021-04-12 11:15:25 +0530

    Fix Twitter Card Image (#24)
    fix twitter card image

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,19 +1,28 @@
 export default {
-    defaultTitle: 'readme.so - Easiest Way to Create A README',
-    description: `Use readme.so's markdown editor and ready made templates to easily create a simple README for your repositories`,
-    openGraph: {
-        type: 'website',
-        locale: 'en_US',
-        url: 'https://readme.so/',
-        title: 'readme.so - Easiest Way to Create A README',
-        description: `Use readme.so's markdown editor and ready made templates to easily create a simple README for your repositories`,
-        image: 'https://github.com/katherinepeterson/readme.so/blob/main/public/readme.png',
-        site_name: 'readme.so',
-        imageWidth: 1200,
-        imageHeight: 1200
-    },
-    twitter: {
-        handle: '@katherinecodes',
-        cardType: 'summary_large_image'
-    }
+	defaultTitle: 'readme.so - Easiest Way to Create A README',
+	description: `Use readme.so's markdown editor and ready made templates to easily create a simple README for your repositories`,
+	additionalMetaTags: [{
+		property: 'keywords',
+		content: 'readme, easiest, create, markdown, git, github, gitlab'
+	}],
+	openGraph: {
+		type: 'website',
+		locale: 'en_US',
+		url: 'https://readme.so/',
+		title: 'readme.so - Easiest Way to Create A README',
+		description: `Use readme.so's markdown editor and ready made templates to easily create a simple README for your repositories`,
+		images: [{
+			url: 'https://readme.so/readme.png',
+			width: 375,
+			height: 375,
+			alt: 'Og Image Alt',
+		}],
+		site_name: 'readme.so',
+		imageWidth: 1200,
+		imageHeight: 1200
+	},
+	twitter: {
+		handle: '@katherinecodes',
+		cardType: 'summary'
+	}
 };
