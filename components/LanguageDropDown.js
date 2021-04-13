9c33c0e73e1ba6d725ed90304c739aa581e2ca9d
Rohit Mathew <rohit.mathew@turtlemint.com>
2021-04-14 01:55:58 +0530

    Move Language Drop Down to components
    Language Drop Down code is moved from index.js HTML render portion to a separate component in /components

---
+++ components/LanguageDropDown.js
@@ -0,0 +1,49 @@
+import { useTranslation } from 'next-i18next'
+
+export function LanguageDropDown({ posts }) {
+    const { t } = useTranslation('common')
+
+    return (
+        <div className="group inline-block relative">
+            <button
+                className="text-gray-800 py-2 px-4 rounded inline-flex items-center"
+            >
+                <span className="mr-1">{t('language')}</span>
+                <svg
+                    className="fill-current h-4 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                >
+                    <path
+                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                    />
+                </svg>
+            </button>
+            <div id='container'></div>
+            <ul className="absolute hidden text-gray-800 pt-1 group-hover:block">
+                <li className="">
+                    <a
+                        className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                        href="/"
+                    >{t('english')}</a
+                    >
+                </li>
+                <li className="">
+                    <a
+                        className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                        href="/fr"
+                    >{t('french')}</a
+                    >
+                </li>
+                <li className="">
+                    <a
+                        className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                        href="/es"
+                    >{t('spanish')}</a
+                    >
+                </li>
+            </ul>
+        </div>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+import { LanguageDropDown } from '../components/LanguageDropDown'
+
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
@@ -93,45 +95,7 @@ const Home = () => {
                 </div>
 
                 <div className="relative items-center flex-1 md:absolute md:inset-y-0 md:right-0">
-                  <div className="group inline-block relative">
-                    <button
-                      className="text-gray-800 py-2 px-4 rounded inline-flex items-center"
-                    >
-                      <span className="mr-1">{t('language')}</span>
-                      <svg
-                        className="fill-current h-4 w-3"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                        />
-                      </svg>
-                    </button>
-                    <ul className="absolute hidden text-gray-800 pt-1 group-hover:block">
-                      <li className="">
-                        <a
-                          className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                          href="/"
-                        >{t('english')}</a
-                        >
-                      </li>
-                      <li className="">
-                        <a
-                          className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                          href="/fr"
-                        >{t('french')}</a
-                        >
-                      </li>
-                      <li className="">
-                        <a
-                          className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                          href="/es"
-                        >{t('spanish')}</a
-                        >
-                      </li>
-                    </ul>
-                  </div>
+                  <LanguageDropDown/>
                 </div>
               </nav>
             </div>
