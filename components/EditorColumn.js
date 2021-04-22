39acf76ce76f20eb8a67386686e754b6a457e2da
MicahBCode <mischa.binder@outlook.de>
2021-04-22 21:16:18 +0200

    added shortcut support for KeyE to focus on editor

--- components/EditorColumn.js
+++ components/EditorColumn.js
@@ -1,5 +1,5 @@
 import Editor from '@monaco-editor/react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export const EditorColumn = ({ focusedSectionSlug, templates, setTemplates }) => {
   const getMarkdown = () => {
@@ -8,6 +8,7 @@ export const EditorColumn = ({ focusedSectionSlug, templates, setTemplates }) =>
   }
   const [markdown, setMarkdown] = useState(getMarkdown())
   const [toggleState, setToggleState] = useState({ theme: 'vs-dark', img: 'toggle_sun.svg' })
+  const editorRef = useRef(null);
 
   useEffect(() => {
     const markdown = getMarkdown()
@@ -30,6 +31,28 @@ export const EditorColumn = ({ focusedSectionSlug, templates, setTemplates }) =>
     toggleDarkMode(toggleState, setToggleState)
   }
 
+  const handleHotkey = ((event) => {
+    const { code, ctrlKey, repeat } = event;
+    if(!editorRef.current.hasWidgetFocus())
+      event.preventDefault();
+
+    if(code === "KeyE" && repeat == false && ctrlKey === false) {
+      editorRef.current.focus();
+    }
+  })
+
+  const handleEditorDidMount = ((editor) => {
+    editorRef.current = editor;
+  })
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleHotkey);
+
+    return () => {
+      window.removeEventListener("keydown", handleHotkey);
+    }
+  }, [handleHotkey]);
+
   return (
     <div className="w-1/2 px-3 full-screen">
       <h3 className="border-transparent text-emerald-500 whitespace-nowrap px-1 border-b-2 font-medium text-sm focus:outline-none">
@@ -44,19 +67,20 @@ export const EditorColumn = ({ focusedSectionSlug, templates, setTemplates }) =>
       </h3>
       {focusedSectionSlug ? (
         <Editor
-          wrapperClassName="rounded-sm border border-gray-500"
-          className="full-screen" // By default, it fully fits with its parent
-          theme={toggleState.theme}
-          language="markdown"
-          value={markdown}
-          onChange={onEdit}
-          loading={'Loading...'}
-          options={{
-            minimap: {
-              enabled: false,
-            },
-            lineNumbers: false,
-          }}
+        onMount={handleEditorDidMount}
+        wrapperClassName="rounded-sm border border-gray-500"
+        className="full-screen" // By default, it fully fits with its parent
+        theme={toggleState.theme}
+        language="markdown"
+        value={markdown}
+        onChange={onEdit}
+        loading={'Loading...'}
+        options={{
+          minimap: {
+            enabled: false,
+          },
+          lineNumbers: false,
+        }}
         />
       ) : (
         <p className="font-sm text-emerald-500 max-w-[16rem] mx-auto mt-10">
