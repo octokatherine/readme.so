814c6c790f20ac98c67bdc42f40c83e7f93fb7cc
pedrozebra <pdellanotte@gmail.com>
2021-04-23 23:21:28 +0200

    Add it to LanguageDropDown Component

--- components/LanguageDropDown.js
+++ components/LanguageDropDown.js
@@ -41,6 +41,13 @@ export function LanguageDropDown({ posts }) {
                     >{t('spanish')}</a
                     >
                 </li>
+				<li className="">
+                    <a
+                        className="w-32 bg-gray-200 hover:bg-emerald-400 py-2 px-4 block whitespace-no-wrap"
+                        href="/it"
+                    >{t('italian')}</a
+                    >
+                </li>
                 <li className="">
                     <a
                         className="w-32 bg-gray-200 hover:bg-emerald-400 text-xs break-words py-2 px-4 block whitespace-no-wrap"
