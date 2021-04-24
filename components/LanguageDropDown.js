4e7e4e528ce5741c6bea7bccbef23123b98b18dc
Sergey Romanenko <sergey.romanenko@flextype.org>
2021-04-24 06:56:00 +0300

    add russian locale

--- components/LanguageDropDown.js
+++ components/LanguageDropDown.js
@@ -44,7 +44,7 @@ export function LanguageDropDown({ posts }) {
                 <li className="">
                      <a
                          className="w-32 bg-gray-200 hover:bg-emerald-400 py-2 px-4 block whitespace-no-wrap"
-                         href="/pt"
+                         href="/ru"
                      >{t('russian')}</a
                      >
                 </li>
