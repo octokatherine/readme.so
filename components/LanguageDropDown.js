956eae10a992611328aba656e223149b3b45228d
allancolibao <colibaoallanreyes@gmail.com>
2021-04-27 00:19:04 +0800

    removed filipino translation

--- components/LanguageDropDown.js
+++ components/LanguageDropDown.js
@@ -51,14 +51,6 @@ export function LanguageDropDown({ posts }) {
         <li className="">
           <a
             className="w-32 bg-gray-200 hover:bg-emerald-400 py-2 px-4 block whitespace-no-wrap"
-            href="/fil"
-          >
-            Filipino
-          </a>
-        </li>
-        <li className="">
-          <a
-            className="w-32 bg-gray-200 hover:bg-emerald-400 py-2 px-4 block whitespace-no-wrap"
             href="/tur"
           >
             Türkçe
--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr', 'es', 'it', 'fil', 'tur'],
+    locales: ['en', 'fr', 'es', 'it', 'tur'],
   },
 }
