b3e7cda1848de3d5df7c51305ffc58cbcbaf838e
katherinepeterson <katherine.peterson@yahoo.com>
2021-04-28 21:39:36 -0700

    Remove netherlands duplicate in dropdown

--- components/LanguageDropDown.js
+++ components/LanguageDropDown.js
@@ -35,23 +35,23 @@ export function LanguageDropDown({ posts }) {
   const { t } = useTranslation('common')
 
   return (
-    <div className="group inline-block relative">
-      <button className="text-gray-800 py-2 px-4 rounded inline-flex items-center">
+    <div className="relative inline-block group">
+      <button className="inline-flex items-center px-4 py-2 text-gray-800 rounded">
         <span className="mr-1">{t('language')}</span>
         <svg
-          className="fill-current h-4 w-3"
+          className="w-3 h-4 fill-current"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
         >
           <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
         </svg>
       </button>
-      <ul className="absolute hidden text-gray-800 pt-1 group-hover:block">
+      <ul className="absolute hidden pt-1 text-gray-800 group-hover:block">
         {languageSupports.map(({ label, route }, index) => {
           return (
             <li key={`${label}_${index}`} className="">
               <a
-                className="w-32 bg-gray-200 hover:bg-emerald-400 py-2 px-4 block whitespace-no-wrap"
+                className="block w-32 px-4 py-2 whitespace-no-wrap bg-gray-200 hover:bg-emerald-400"
                 href={route}
               >
                 {label}
@@ -61,15 +61,7 @@ export function LanguageDropDown({ posts }) {
         })}
         <li className="">
           <a
-            className="w-32 bg-gray-200 hover:bg-emerald-400 py-2 px-4 block whitespace-no-wrap"
-            href="/nl"
-          >
-            Nederlands
-          </a>
-        </li>
-        <li className="">
-          <a
-            className="w-32 bg-gray-200 hover:bg-emerald-400 text-xs break-words py-2 px-4 block whitespace-no-wrap"
+            className="block w-32 px-4 py-2 text-xs break-words whitespace-no-wrap bg-gray-200 hover:bg-emerald-400"
             href="https://github.com/katherinepeterson/readme.so/issues/new"
             target="_blank"
           >
