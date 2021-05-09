330aae720fcf361b11bb59f011ce2c63352bcfac
Vishnumohan R K <mohanvish1999@gmail.com>
2021-05-09 19:04:41 +0530

    modify <LanguageDropDown /> (#110)

--- components/LanguageDropDown.js
+++ components/LanguageDropDown.js
@@ -46,24 +46,23 @@ export function LanguageDropDown({ posts }) {
           <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
         </svg>
       </button>
-      <ul className="absolute hidden pt-1 text-gray-800 group-hover:block">
-        {languageSupports.map(({ label, route }, index) => {
-          return (
-            <li key={`${label}_${index}`} className="">
-              <a
-                className="block w-32 px-4 py-2 whitespace-no-wrap bg-gray-200 hover:bg-emerald-400"
-                href={route}
-              >
-                {label}
-              </a>
-            </li>
-          )
-        })}
+      <ul className="absolute text-gray-800 pt-1 origin-top transform-gpu transition-transform scale-y-0 group-hover:scale-y-100 focus-within:scale-y-100">
+        {languageSupports.map(({ label, route }, index) => (
+          <li key={`${label}_${index}`} className="">
+            <a
+              className="block w-32 px-4 py-2 whitespace-no-wrap bg-gray-200 hover:bg-emerald-400"
+              href={route}
+            >
+              {label}
+            </a>
+          </li>
+        ))}
         <li className="">
           <a
             className="block w-32 px-4 py-2 text-xs break-words whitespace-no-wrap bg-gray-200 hover:bg-emerald-400"
             href="https://github.com/katherinepeterson/readme.so/issues/new"
             target="_blank"
+            rel="noopener noreferrer"
           >
             {t('translation-error')}
           </a>
