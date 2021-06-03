2a1057b903df04ac80665aa8bc7a99f4734203c9
fourleaf <gundamwing.wei@gmail.com>
2021-06-03 22:11:34 +0800

    ✨ feat(i18n): add zh_CN templates

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
-import { tr_TR } from './section-templates-tr_TR'
 import { en_EN } from './section-templates-en_EN'
+import { tr_TR } from './section-templates-tr_TR'
+import { zh_CN } from './section-templates-zh_CN'
 
 module.exports = {
   tr: tr_TR,
   en: en_EN,
+  zh: zh_CN,
 }
---
+++ data/section-templates-zh_CN.js
@@ -0,0 +1,374 @@
+export const zh_CN = [
+  {
+    slug: 'title-and-description',
+    name: '标题与描述',
+    markdown: `
+# 项目标题
+
+简要描述项目的标题与使用者
+
+`,
+  },
+  {
+    slug: 'installation',
+    name: '安装',
+    markdown: `
+## 安装 
+
+使用 npm 安装 \`my-project\`
+
+\`\`\`bash 
+  npm install my-project
+  cd my-project
+\`\`\`
+    `,
+  },
+  {
+    slug: 'logo',
+    name: '标志',
+    markdown: `
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
+    `,
+  },
+  {
+    slug: 'run-locally',
+    name: '本地运行',
+    markdown: `
+## 本地运行
+
+克隆这个项目
+
+\`\`\`bash
+  git clone https://link-to-project
+\`\`\`
+
+进入项目目录
+
+\`\`\`bash
+  cd my-project
+\`\`\`
+
+安装依赖项
+
+\`\`\`bash
+  npm install
+\`\`\`
+
+启动服务
+
+\`\`\`bash
+  npm run start
+\`\`\`
+
+  `,
+  },
+  {
+    slug: 'screenshots',
+    name: '截图',
+    markdown: `
+## 截图
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+  `,
+  },
+  {
+    slug: 'env-variables',
+    name: '环境变量',
+    markdown: `
+## 环境变量
+
+想要运行这个项目你需要添加如下环境变量到 .env 文件
+
+\`API_KEY\`
+
+\`ANOTHER_API_KEY\`
+
+  `,
+  },
+  {
+    slug: 'features',
+    name: '特性',
+    markdown: `
+## 特性
+
+- 明亮/暗色 模式切换
+- 实时预览
+- 全屏模式
+- 跨平台
+
+  `,
+  },
+  {
+    slug: 'usage-examples',
+    name: '用法/范例',
+    markdown: `
+## 用法/范例
+
+\`\`\`javascript
+import Component from 'my-project'
+
+function App() {
+  return <Component />
+}
+\`\`\`
+
+  `,
+  },
+  {
+    slug: 'api',
+    name: 'API 参考',
+    markdown: `
+## API 参考
+
+#### 获取所有元素
+
+\`\`\`http
+  GET /api/items
+\`\`\`
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| \`api_key\` | \`string\` | **Required**. Your API key |
+
+#### 获取一个元素
+
+\`\`\`http
+  GET /api/items/$\{id}
+\`\`\`
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| \`id\`      | \`string\` | **Required**. Id of item to fetch |
+
+#### add(num1, num2)
+
+获取两数相加之和
+
+  `,
+  },
+  {
+    slug: 'contributing',
+    name: '贡献',
+    markdown: `
+## 贡献
+
+一直欢迎新的共享
+
+查看 \`contributing.md\` 来开始.
+
+请保持这个项目的 \`行为守则\`.
+
+  `,
+  },
+  {
+    slug: 'tests',
+    name: '运行测试',
+    markdown: `
+## 运行测试
+
+使用一下命令运行测试
+
+\`\`\`bash
+  npm run test
+\`\`\`
+
+  `,
+  },
+  {
+    slug: 'license',
+    name: '许可证',
+    markdown: `
+## 许可证
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+  `,
+  },
+  {
+    slug: 'badges',
+    name: '徽章',
+    markdown: `
+## 徽章
+
+添加从什么地方来的徽章,比如: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+  `,
+  },
+  {
+    slug: 'roadmap',
+    name: '路线图',
+    markdown: `
+## 路线图
+
+- 补充浏览器支持
+
+- 添加更多集成
+
+  `,
+  },
+  {
+    slug: 'authors',
+    name: '作者',
+    markdown: `
+## 作者
+
+- [@katherinepeterson](https://www.github.com/katherinepeterson)
+
+  `,
+  },
+  {
+    slug: 'acknowledgement',
+    name: 'Acknowledgements',
+    markdown: `
+## 致谢
+
+ - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
+ - [Awesome README](https://github.com/matiassingers/awesome-readme)
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+  `,
+  },
+  {
+    slug: 'support',
+    name: '支持',
+    markdown: `
+## 支持
+
+发送 email 到 fake@fake.com 或者加入我们的 Slack 频道来获得支持
+
+  `,
+  },
+  {
+    slug: 'feedback',
+    name: '反馈',
+    markdown: `
+## 反馈
+
+如果你有什么反馈请使用 fake@fake.com 联系我们
+
+  `,
+  },
+  {
+    slug: 'related',
+    name: '关联',
+    markdown: `
+## 关联
+
+这里有一些关联项目
+
+[Awesome README](https://github.com/matiassingers/awesome-readme)
+
+  `,
+  },
+  {
+    slug: 'demo',
+    name: '演示',
+    markdown: `
+## 演示
+
+插入一个用于演示的动态图或链接
+
+  `,
+  },
+  {
+    slug: 'tech',
+    name: '技术',
+    markdown: `
+## 技术栈
+
+**客户端:** React, Redux, TailwindCSS
+
+**服务端:** Node, Express
+
+  `,
+  },
+  {
+    slug: 'optimizations',
+    name: '优化',
+    markdown: `
+## 优化
+
+你在项目里进行了什么优化么? 比如: 重构, 性能改进, 可访问性优化
+
+  `,
+  },
+  {
+    slug: 'lessons',
+    name: '经验教训',
+    markdown: `
+## 得到的经验教训
+
+你从构建这个项目中获得了什么经验教学么? 你遇到了哪些挑战,你又是怎么克服他们的?
+
+  `,
+  },
+  {
+    slug: 'faq',
+    name: '常见问题',
+    markdown: `
+## 常见问题
+
+#### 问题 1
+
+答案 1
+
+#### 问题 2
+
+答案 2
+
+  `,
+  },
+  {
+    slug: 'used-by',
+    name: '使用者',
+    markdown: `
+## 使用者
+
+这个项目被如下公司使用:
+
+- 公司 1
+- 公司 2
+
+  `,
+  },
+  {
+    slug: 'documentation',
+    name: '文档',
+    markdown: `
+## 文档
+
+[文档](https://linktodocumentation)
+
+  `,
+  },
+  {
+    slug: 'deployment',
+    name: '部署',
+    markdown: `
+## 部署
+
+运行如下命令部署项目
+
+\`\`\`bash
+  npm run deploy
+\`\`\`
+
+  `,
+  },
+  {
+    slug: 'appendix',
+    name: '附录',
+    markdown: `
+## 附录
+
+在这里添加附加信息
+
+  `,
+  },
+]
