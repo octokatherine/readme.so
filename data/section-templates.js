1104889751ff3eda79cee9c007a32f20ab6ab1ec
Nick Oelsner <noelsner@gmail.com>
2021-06-02 18:57:56 -0700

    add section with various other possible sections

--- data/section-templates.js
+++ data/section-templates.js
@@ -389,9 +389,33 @@ Javascript, HTML, CSS...
     slug: 'github-profile-links',
     name: 'Github Profile - Links',
     markdown: `
-[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fie&logoColor=white)](https://katherinempeterson.com/)
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherinempeterson.com/)
 [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/)
 [![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/)
+
+  `,
+  },
+  {
+    slug: 'github-profile-other',
+    name: 'Github Profile - Other',
+    markdown: `
+## Other Common Github Profile Sections
+👩‍💻 I'm currently working on...
+
+🧠 I'm currently learning...
+
+👯‍♀️ I'm looking to collaborate on...
+
+🤔 I'm looking for help with...
+
+💬 Ask me about...
+
+📫 How to reach me...
+
+😄 Pronouns...
+
+⚡️ Fun fact...
 
   `,
   },
