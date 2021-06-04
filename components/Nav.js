00aac6e31fe94ebc7afa59ea7d98547697421cd3
Rishabh-Rathod <rishabhrathod2012@gmail.com>
2021-06-05 03:02:51 +0530

    Add download button in Nav

--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
 import Menu from './icons/Menu'
 import Close from './icons/Close'
+import useDeviceDetect from '../hooks/useDeviceDetect'
 
 export const Nav = ({
   selectedSectionSlugs,
@@ -15,12 +16,17 @@ export const Nav = ({
     return `${acc}${template.markdown}`
   }, ``)
 
+  const { isMobile } = useDeviceDetect()
+
   const downloadMarkdownFile = () => {
     const a = document.createElement('a')
     const blob = new Blob([markdown])
     a.href = URL.createObjectURL(blob)
     a.download = 'README.md'
     a.click()
+    if (isMobile && isDrawerOpen) {
+      onMenuClick()
+    }
     setShowModal(true)
   }
 
@@ -34,27 +40,29 @@ export const Nav = ({
           <span className="ml-2 text-white logo text-md">readme.so</span>
         </a>
       </Link>
-      {/* visible for sm only */}
-      <button
-        className="focus:outline-none focus:ring-2 focus:ring-emerald-400"
-        onClick={onMenuClick}
-      >
-        {isDrawerOpen ? (
-          <Close className="w-10 h-10 md:hidden fill-current text-emerald-500" />
-        ) : (
-          <Menu className="w-10 h-10 md:hidden fill-current text-emerald-500" />
-        )}
-      </button>
-      {/* visible for md and above */}
-      <button
-        type="button"
-        aria-label="Download Markdown"
-        className="hidden md:inline-flex relative  items-center px-4 py-2 text-sm font-bold tracking-wide text-white border border-transparent rounded-md shadow-sm bg-emerald-500 hover:bg-emerald-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-emerald-500"
-        onClick={downloadMarkdownFile}
-      >
-        <img className="w-auto h-6 cursor-pointer" src="download.svg" />
-        <span className=" ml-2 ">{t('nav-download')}</span>
-      </button>
+      <div className="flex flex-row-reverse md:flex-row">
+        {/* visible for sm only */}
+        <button
+          className="focus:outline-none focus:ring-2 focus:ring-emerald-400"
+          onClick={onMenuClick}
+        >
+          {isDrawerOpen ? (
+            <Close className="w-10 h-10 md:hidden fill-current text-emerald-500" />
+          ) : (
+            <Menu className="w-10 h-10 md:hidden fill-current text-emerald-500" />
+          )}
+        </button>
+        {/* visible for md and above */}
+        <button
+          type="button"
+          aria-label="Download Markdown"
+          className="flex flex-row relative items-center mr-4 md:mr-0 px-4 py-2 text-sm font-bold tracking-wide text-white border border-transparent rounded-md shadow-sm bg-emerald-500 hover:bg-emerald-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-emerald-500"
+          onClick={downloadMarkdownFile}
+        >
+          <img className="w-auto h-6 cursor-pointer" src="download.svg" />
+          <span className="hidden md:inline-block ml-2">{t('nav-download')}</span>
+        </button>
+      </div>
     </nav>
   )
 }
