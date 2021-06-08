2b0b830be9a90cb5a00b9ee1af8cf29dd69c98ef
yikayiyo <837937787@qq.com>
2021-06-08 16:16:18 +0800

    Add simplified Chinese support; fix #158; a little change to pass a test.

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,18 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="katherinecodes"
+        data-description="Support me on Buy me a coffee!"
+        data-message=""
+        data-color="#FFDD00"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+      ></script>
     </>
   )
 }
