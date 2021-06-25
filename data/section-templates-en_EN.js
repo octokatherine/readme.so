9aad0232e6d9b5ad166dcf1c29202d3454ff40ad
daniloab <daniloassis.ti@gmail.com>
2021-06-25 13:09:07 -0300

    feat(section): new table content section with heading and subheanding

--- data/section-templates-en_EN.js
+++ data/section-templates-en_EN.js
@@ -371,4 +371,20 @@ Any additional information goes here
 
   `,
   },
+  {
+    slug: 'table-content',
+    name: 'Table of Content',
+    markdown: `
+## Table of Content
+
+- [1. Introduction](#1-introduction)
+  * [1.1 Purpose](#11-purpose)
+  * [1.2 Scope](#12-scope)
+  * [1.3 Acronyms](#13-acronyms)
+  * [1.4 References](#14-references)
+    + [1.4.1 Internal References](#141-internal-references)
+    + [1.4.2 External References](#142-external-references)
+  * [1.5 Overview](#15-overview)
+  `,
+  },
 ]
