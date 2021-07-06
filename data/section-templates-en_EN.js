f77029d0eabbf768d5df2b0516edcae73ff2939f
conner <59223342+Intevel@users.noreply.github.com>
2021-07-06 16:24:21 +0200

    Added Color Reference (#161)

--- data/section-templates-en_EN.js
+++ data/section-templates-en_EN.js
@@ -371,4 +371,16 @@ Any additional information goes here
 
   `,
   },
+  {
+    slug: 'colorreference',
+    name: 'Color Reference',
+    markdown: `## Color Reference
+
+| Color             | Hex                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| Example Color | ![#0a192f](https://via.placeholder.com/10/0a192f?text=+) #0a192f |
+| Example Color | ![#f8f8f8](https://via.placeholder.com/10/f8f8f8?text=+) #f8f8f8 |
+| Example Color | ![#00b48a](https://via.placeholder.com/10/00b48a?text=+) #00b48a |
+| Example Color | ![#00d1a0](https://via.placeholder.com/10/00b48a?text=+) #00d1a0 | `,
+  },
 ]
