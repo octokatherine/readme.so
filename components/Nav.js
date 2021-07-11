40612314908c8e76c6ab0237c59485175fa4a141
Nick Oelsner <56660727+noelsner@users.noreply.github.com>
2021-07-10 22:25:32 -0700

    replace logo with new # logo, replace screenshot on index.js (#177)

--- components/Nav.js
+++ components/Nav.js
@@ -36,8 +36,7 @@ export const Nav = ({
     <nav className="flex justify-between p-4 bg-gray-800 align-center w-full">
       <Link href="/">
         <a className="focus:outline-none focus:ring-2 focus:ring-emerald-400 flex items-center">
-          <img className="w-auto h-8" src="readme.svg" alt="readme.so logo" />
-          <span className="ml-2 text-white logo text-md">readme.so</span>
+          <img className="w-auto h-12" src="readme.svg" alt="readme.so logo" />
         </a>
       </Link>
       <div className="flex flex-row-reverse md:flex-row">
