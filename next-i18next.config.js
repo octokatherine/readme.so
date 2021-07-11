ab44a48304a49da34a03181b63db3f2e566cfa14
eric <eric.chang@promeritage.com.tw>
2021-07-11 17:22:16 +0800

    removeZhcn

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr', 'es', 'it', 'fil', 'tr', 'nl', 'de', 'pt', 'zhcn', 'zhtw'],
+    locales: ['en', 'fr', 'es', 'it', 'fil', 'tr', 'nl', 'de', 'pt', 'zhtw'],
   },
 }
