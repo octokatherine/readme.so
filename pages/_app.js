ac897e1120d225a087d3c5c13316e68c253433e3
Max Programming <ui.sabuwala@gmail.com>
2021-07-24 16:49:18 +0530

    ✨ Added progress bar while navigating

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,12 @@ import { DefaultSeo } from 'next-seo'
 import { appWithTranslation } from 'next-i18next'
 
 import SEO from '../next-seo.config.js'
+import NextNProgress from 'nextjs-progressbar'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
+      <NextNProgress color="#10b981" height={4} />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
