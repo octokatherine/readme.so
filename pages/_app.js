7ea531caca4d8191afa63084e74de35162299aeb
Max Programming <ui.sabuwala@gmail.com>
2021-08-05 12:23:06 +0530

    ♻️ Import and use tailwind'c colors

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,12 @@ import { appWithTranslation } from 'next-i18next'
 
 import SEO from '../next-seo.config.js'
 import NextNProgress from 'nextjs-progressbar'
+import { emerald } from 'tailwindcss/colors'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <NextNProgress color="#10b981" height={4} />
+      <NextNProgress color={emerald[500]} height={4} />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
