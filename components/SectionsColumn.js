e7ddbaf7379b57c8d3f09833797b63d238b7e672
Allister Grange <allistergrange@gmail.com>
2021-11-15 19:40:36 +1300

    Fixed compilation warning fron nextJs

--- components/SectionsColumn.js
+++ components/SectionsColumn.js
@@ -255,7 +255,7 @@ export const SectionsColumn = ({
             fill="currentColor"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
             />
           </svg>
