e7fab0e705def8c4f4726ccec613cac4aff5d688
Xuan <97ssps30212@gmail.com>
2022-04-21 02:08:55 +0800

    Translate templates to traditional Chinese

--- data/index.js
+++ data/index.js
@@ -2,10 +2,12 @@ import { tr_TR } from './section-templates-tr_TR'
 import { en_EN } from './section-templates-en_EN'
 import { pt_BR } from './section-templates-pt_BR'
 import { cn_CN } from './section-templates-cn_CN'
+import { zh_TW } from './section-templates-zh_TW'
 
 module.exports = {
   tr: tr_TR,
   en: en_EN,
   pt: pt_BR,
   cn: cn_CN,
+  tw: zh_TW,
 }
