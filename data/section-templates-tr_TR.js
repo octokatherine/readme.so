9961375a01aa2b2a05d71b2e74d47e6014b0e425
SumitNalavade <vs.nalavade2003@gmail.com>
2022-08-09 18:42:11 -0500

    Fix Bug #253: Replaced link for MIT License badge

--- data/section-templates-tr_TR.js
+++ data/section-templates-tr_TR.js
@@ -194,7 +194,7 @@ Testleri çalıştırmak için aşağıdaki komutu çalıştırın
 
 [shields.io](https://shields.io/) Gibi bir yerden rozetler ekleyin.
 
-[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
 [![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
 [![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
 
